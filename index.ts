847693aca92e7ad08d9b1e339f534ac9ee3f0988
Kashif Mumtaz <139809447+Kashif8286@users.noreply.github.com>
2023-07-22 01:00:17 +0500

    Add files via upload

--- index.ts
+++ index.ts
@@ -1,7 +1,27 @@
-//Personal Message: Store a person’s name in a variable, and print a message to that person. Your message should be simple, such as, “Hello Eric, would you like to learn some Python today?”
+// Name Cases: Store a person’s name in a variable, and then print that person’s name in lowercase, uppercase, and titlecase.
 
+let personName: string = "Kashif Mumtaz Ansari"
+ 
+// option 1 show in lowercase:
 
-let personName: string = "Ansari"
-let message: string = `Hi ${personName}, hope you are well and good`
-console.log(message)
+let lowercaseName: string = personName.toLowerCase();
 
+console.log(lowercaseName)
+
+// option 2 show in uppercase:
+
+let uppercaseName: string = personName.toUpperCase();
+
+console.log(uppercaseName)
+
+// option 3 show in titlecase:
+
+function titlecase(personName:string){
+    let name;
+    name = personName.toLowerCase().split(" ")
+    for(let i = 0;i < name.length;i++){
+        name[i] = name[i].charAt(0).toUpperCase() +name[i].slice(1);
+    }
+return name.join(" ")
+}
+console.log(`Titlecase: ${titlecase("Kashif Mumtaz Ansari")}`)
